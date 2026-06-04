d431cdc4a0939780f67827c8b0c49efaf1e2dde4
Abinash-25-hue <shivanand1.july.22@gmail.com>
2026-06-04 23:36:02 +0530

    Added the first function.
    This function prints a specific pattern.

--- include/codesaathi.hpp
+++ include/codesaathi.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
+#include<iostream>
+using namespace std;
+
 inline int print_pattern_1 (int lines)
 {
-    for (;;)
+    for (int index = 0; index < lines; index++)
     {
-
+        for (int jindex = 0; jindex < index; jindex++)
+        {
+            cout<< "*";
+        }
+        cout<< endl;
     }
 }
